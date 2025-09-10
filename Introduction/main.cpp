ffb2af4c9c4b2481e6181f13c22227277b0e467a
RahafJazar <rahafjazar73@gmail.com>
2025-09-10 18:00:52 +0300

    C++

---
+++ Introduction/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Rahaf Jazar" << " aya" << std::endl
+              << std::endl;
+    std::cout << "Mohammed" << "safi " << std::endl;
+
+    std::cout << 3 + 5 << std::endl;
+
+    // Homework1
+    std::cout << "***********************************\n";
+    std::cout << "Name :Rahaf Bassam Jazar\n";
+    std::cout << "Age: 44 Years\n";
+    std::cout << "City:Ammam\n";
+    std::cout << "Country:Jordan" << std::endl;
+    std::cout << "**********************************\n";
+
+    //_________________________________________________
+
+    std::cout << "\n\n****************************\n";
+    std::cout << "****************************\n";
+    std::cout << "****************************\n\n\n";
+
+    //__________________________________________________
+    std::cout << "I Love Programming\n\n";
+    std::cout << "I Promise to be the best developer ever\n\n";
+    std::cout << "I know it will take some time to practice, but i will achieve my goal\n\n";
+    std::cout << "Best Regards,\nRahaf Jazar";
+}
