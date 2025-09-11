ad936c255dc3da383bf4363da832422ce746f6f9
RahafJazar <rahafjazar73@gmail.com>
2025-09-11 12:11:05 +0300

    literals and Escape sequences

--- Introduction/main.cpp
+++ Introduction/main.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Rahaf Jazar" << " aya" << std::endl
-              << std::endl;
-    std::cout << "Mohammed" << "safi " << std::endl;
+    cout << "Rahaf Jazar" << " aya" << std::endl
+         << std::endl;
+    cout << "Mohammed" << "safi " << std::endl;
 
-    std::cout << 3 + 5 << std::endl;
+    cout << 3 + 5 << std::endl;
 
     // Homework1
-    std::cout << "***********************************\n";
-    std::cout << "Name :Rahaf Bassam Jazar\n";
-    std::cout << "Age: 44 Years\n";
-    std::cout << "City:Ammam\n";
-    std::cout << "Country:Jordan" << std::endl;
-    std::cout << "**********************************\n";
+    cout << "***********************************\n";
+    cout << "Name :Rahaf Bassam Jazar\n";
+    cout << "Age: 44 Years\n";
+    cout << "City:Ammam\n";
+    cout << "Country:Jordan" << std::endl;
+    cout << "**********************************\n";
 
     //_________________________________________________
 
-    std::cout << "\n\n****************************\n";
-    std::cout << "****************************\n";
-    std::cout << "****************************\n\n\n";
+    cout << "\n\n****************************\n";
+    cout << "****************************\n";
+    cout << "****************************\n\n\n";
 
     //__________________________________________________
-    std::cout << "I Love Programming\n\n";
-    std::cout << "I Promise to be the best developer ever\n\n";
-    std::cout << "I know it will take some time to practice, but i will achieve my goal\n\n";
-    std::cout << "Best Regards,\nRahaf Jazar";
+    cout << "I Love Programming\n\n";
+    cout << "I Promise to be the best developer ever\n\n";
+    cout << "I know it will take some time to practice, but i will achieve my goal" << std::endl
+         << std::endl;
+    cout << "Best Regards,\nRahaf Jazar\n";
+
+    //__________________________________________________
+    cout << "*   *" << std::endl;
+    cout << "*   *" << std::endl;
+    cout << "*****" << std::endl;
+    cout << "*   *" << std::endl;
+    cout << "*   *" << std::endl;
+
+    // Homework2
+
+    cout << "\a \n";
+    //_______________________________________________
+    cout << "Dear Sir \\Madam\n\n";
+    cout << "How are You ? \n\n";
+    cout << "My name is \"Mohammed\" ,nice to meet you .\n\n";
+
+    //______________________________________________
+    cout << "Ali\tAhmed\tLina\n";
+    cout << "Fadi\tZain\tMona\n";
 }
