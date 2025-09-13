90b028f938fd0d61fdb14faf1b3fbe509b56f104
Rahaf Jazar <145443862+RahafJazar@users.noreply.github.com>
2025-09-13 18:40:43 +0300

    MyCOmputerCode was added

---
+++ MyCOmputerCode/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Rahaf Jazar" << " aya" << std::endl
+         << std::endl;
+    cout << "Mohammed" << "safi " << std::endl;
+
+    cout << 3 + 5 << std::endl;
+
+    // Homework1
+    cout << "***********************************\n";
+    cout << "Name :Rahaf Bassam Jazar\n";
+    cout << "Age: 44 Years\n";
+    cout << "City:Ammam\n";
+    cout << "Country:Jordan" << std::endl;
+    cout << "**********************************\n";
+
+    //_________________________________________________
+
+    cout << "\n\n****************************\n";
+    cout << "****************************\n";
+    cout << "****************************\n\n\n";
+
+    //__________________________________________________
+    cout << "I Love Programming\n\n";
+    cout << "I Promise to be the best developer ever\n\n";
+    cout << "I know it will take some time to practice, but i will achieve my goal" << std::endl
+         << std::endl;
+    cout << "Best Regards,\nRahaf Jazar\n";
+
+    //__________________________________________________
+    cout << "*   *" << std::endl;
+    cout << "*   *" << std::endl;
+    cout << "*****" << std::endl;
+    cout << "*   *" << std::endl;
+    cout << "*   *" << std::endl;
+
+    // Homework2
+
+    cout << "\a \n";
+    //_______________________________________________
+    cout << "Dear Sir \\Madam\n\n";
+    cout << "How are You ? \n\n";
+    cout << "My name is \"Mohammed\" ,nice to meet you .\n\n";
+
+    //______________________________________________
+    cout << "Ali\tAhmed\tLina\n";
+    cout << "Fadi\tZain\tMona\n";
+}
