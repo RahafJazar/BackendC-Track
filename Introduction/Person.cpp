e14da41a2631cffcd19b3bdb986208d40bbc5abd
Rahaf Jazar <145443862+RahafJazar@users.noreply.github.com>
2025-09-19 08:23:00 +0300

    solution of Math function homework

--- Introduction/Person.cpp
+++ Introduction/Person.cpp
@@ -4,86 +4,86 @@
 using namespace std;
 int main()
 {
-    // string name = "Rahaf Bassam Jazar";
-    // int age = 44;
-    // string city = "Amman", country = "Jordan";
-    // int monthSalary = 5000, yearSalary = monthSalary * 12;
-    // char gender = 'M';
-    // int yearOfMarriege = 1;
+  // string name = "Rahaf Bassam Jazar";
+  // int age = 44;
+  // string city = "Amman", country = "Jordan";
+  // int monthSalary = 5000, yearSalary = monthSalary * 12;
+  // char gender = 'M';
+  // int yearOfMarriege = 1;
 
-    // // HW1 of variables & User Inputs
+  // // HW1 of variables & User Inputs
 
-    // cout << "Please enter your Name: " << endl;
-    // cin >> name;
-    // cout << "Please enter your Age: " << endl;
-    // cin >> age;
-    // cout << "Please enter your City: " << endl;
-    // cin >> city;
-    // cout << "Please enter your Country: " << endl;
-    // cin >> country;
-    // cout << "Please enter your Monthly Salaty: " << endl;
-    // cin >> monthSalary;
-    // cout << "Please enter your Gender: " << endl;
-    // cin >> gender;
-    // cout << "Please enter your Married: " << endl;
-    // cin >> yearOfMarriege;
+  // cout << "Please enter your Name: " << endl;
+  // cin >> name;
+  // cout << "Please enter your Age: " << endl;
+  // cin >> age;
+  // cout << "Please enter your City: " << endl;
+  // cin >> city;
+  // cout << "Please enter your Country: " << endl;
+  // cin >> country;
+  // cout << "Please enter your Monthly Salaty: " << endl;
+  // cin >> monthSalary;
+  // cout << "Please enter your Gender: " << endl;
+  // cin >> gender;
+  // cout << "Please enter your Married: " << endl;
+  // cin >> yearOfMarriege;
 
-    // cout << "**********************************" << endl;
-    // cout << "Name :" << name << endl;
-    // cout << "Age  :" << age << endl;
-    // cout << "City :" << city << endl;
-    // cout << "Country :" << country << endl;
-    // cout << "Monthly Salary :" << monthSalary << endl;
-    // cout << "Yearly Salary :" << yearSalary << endl;
-    // cout << "Gender :" << gender << endl;
-    // cout << "Married :" << yearOfMarriege << endl;
-    // cout << "**********************************" << endl;
+  // cout << "**********************************" << endl;
+  // cout << "Name :" << name << endl;
+  // cout << "Age  :" << age << endl;
+  // cout << "City :" << city << endl;
+  // cout << "Country :" << country << endl;
+  // cout << "Monthly Salary :" << monthSalary << endl;
+  // cout << "Yearly Salary :" << yearSalary << endl;
+  // cout << "Gender :" << gender << endl;
+  // cout << "Married :" << yearOfMarriege << endl;
+  // cout << "**********************************" << endl;
 
-    // // HW2
-    // int num1 = 20, num2 = 30, num3 = 10;
-    // int totalSum = num1 + num2 + num3;
-    // cout << num1 << "+ \n"
-    //      << num2 << "+ \n"
-    //      << num3 << endl;
-    // cout << "____________________________________" << endl;
-    // cout << "Total = " << totalSum;
+  // // HW2
+  // int num1 = 20, num2 = 30, num3 = 10;
+  // int totalSum = num1 + num2 + num3;
+  // cout << num1 << "+ \n"
+  //      << num2 << "+ \n"
+  //      << num3 << endl;
+  // cout << "____________________________________" << endl;
+  // cout << "Total = " << totalSum;
 
-    // // Hw3
-    // int currentAge = 25;
-    // cout << "After 5 years you will be " << currentAge + 5 << "years old \n";
+  // // Hw3
+  // int currentAge = 25;
+  // cout << "After 5 years you will be " << currentAge + 5 << "years old \n";
 
-    // operators
-    // hw1
+  // operators
+  // hw1
 
-    // short a, b;
-    // cout << "Please enter the first number A ? \n";
-    // cin >> a;
-    // cout << "Please enter the first number B? \n";
-    // cin >> b;
+  // short a, b;
+  // cout << "Please enter the first number A ? \n";
+  // cin >> a;
+  // cout << "Please enter the first number B? \n";
+  // cin >> b;
 
-    // cout << a << '+' << b << '=' << (a + b) << "\n";
-    // cout << a << '-' + b << '=' << (a - b) << "\n";
-    // cout << a << '*' + b << '=' << (a * b) << "\n";
-    // cout << a << '/' + b << '=' << (a / b) << "\n";
-    // cout << a << '%' + b << '=' << (a % b) << "\n";
+  // cout << a << '+' << b << '=' << (a + b) << "\n";
+  // cout << a << '-' + b << '=' << (a - b) << "\n";
+  // cout << a << '*' + b << '=' << (a * b) << "\n";
+  // cout << a << '/' + b << '=' << (a / b) << "\n";
+  // cout << a << '%' + b << '=' << (a % b) << "\n";
 
-    //
+  //
 
-    float loanAmount, numOfMonths;
-    cout << "Please enter Loan Amounts   : \n ";
-    cin >> loanAmount;
-    cout << "Please enter  num of months   : \n ";
-    cin >> numOfMonths;
+  float loanAmount, numOfMonths;
+  cout << "Please enter Loan Amounts   : \n ";
+  cin >> loanAmount;
+  cout << "Please enter  num of months   : \n ";
+  cin >> numOfMonths;
 
-    float loanPerMonth = loanAmount / numOfMonths;
-    cout << "Loan Per Month : " << loanPerMonth << endl;
+  float loanPerMonth = loanAmount / numOfMonths;
+  cout << "Loan Per Month : " << loanPerMonth << endl;
 
-   float rectangleSide;
+  float rectangleSide;
   float rectangleDiagonal;
   cout << "Please enter rectangleSide   : \n ";
-    cin >> loanAmount;
+  cin >> rectangleSide;
   cout << "Please enter rectangleDiagonal  : \n ";
-    cin >> loanAmount;
-
-
+  cin >> rectangleDiagonal;
+  float area = rectangleSide * sqrt(pow(rectangleDiagonal, 2) - pow(rectangleSide, 2));
+  cout << "Area of Rectangle : " << area << endl;
 }
--- MyCOmputerCode/main.cpp
+++ MyCOmputerCode/main.cpp
@@ -1,50 +1,72 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 int main()
 {
-    cout << "Rahaf Jazar" << " aya" << std::endl
-         << std::endl;
-    cout << "Mohammed" << "safi " << std::endl;
+     // cout << "Rahaf Jazar" << " aya" << std::endl
+     //      << std::endl;
+     // cout << "Mohammed" << "safi " << std::endl;
 
-    cout << 3 + 5 << std::endl;
+     // cout << 3 + 5 << std::endl;
 
-    // Homework1
-    cout << "***********************************\n";
-    cout << "Name :Rahaf Bassam Jazar\n";
-    cout << "Age: 44 Years\n";
-    cout << "City:Ammam\n";
-    cout << "Country:Jordan" << std::endl;
-    cout << "**********************************\n";
+     // // Homework1
+     // cout << "***********************************\n";
+     // cout << "Name :Rahaf Bassam Jazar\n";
+     // cout << "Age: 44 Years\n";
+     // cout << "City:Ammam\n";
+     // cout << "Country:Jordan" << std::endl;
+     // cout << "**********************************\n";
 
-    //_________________________________________________
+     // //_________________________________________________
 
-    cout << "\n\n****************************\n";
-    cout << "****************************\n";
-    cout << "****************************\n\n\n";
+     // cout << "\n\n****************************\n";
+     // cout << "****************************\n";
+     // cout << "****************************\n\n\n";
 
-    //__________________________________________________
-    cout << "I Love Programming\n\n";
-    cout << "I Promise to be the best developer ever\n\n";
-    cout << "I know it will take some time to practice, but i will achieve my goal" << std::endl
-         << std::endl;
-    cout << "Best Regards,\nRahaf Jazar\n";
+     // //__________________________________________________
+     // cout << "I Love Programming\n\n";
+     // cout << "I Promise to be the best developer ever\n\n";
+     // cout << "I know it will take some time to practice, but i will achieve my goal" << std::endl
+     //      << std::endl;
+     // cout << "Best Regards,\nRahaf Jazar\n";
 
-    //__________________________________________________
-    cout << "*   *" << std::endl;
-    cout << "*   *" << std::endl;
-    cout << "*****" << std::endl;
-    cout << "*   *" << std::endl;
-    cout << "*   *" << std::endl;
+     // //__________________________________________________
+     // cout << "*   *" << std::endl;
+     // cout << "*   *" << std::endl;
+     // cout << "*****" << std::endl;
+     // cout << "*   *" << std::endl;
+     // cout << "*   *" << std::endl;
 
-    // Homework2
+     // // Homework2
 
-    cout << "\a \n";
-    //_______________________________________________
-    cout << "Dear Sir \\Madam\n\n";
-    cout << "How are You ? \n\n";
-    cout << "My name is \"Mohammed\" ,nice to meet you .\n\n";
+     // cout << "\a \n";
+     // //_______________________________________________
+     // cout << "Dear Sir \\Madam\n\n";
+     // cout << "How are You ? \n\n";
+     // cout << "My name is \"Mohammed\" ,nice to meet you .\n\n";
 
-    //______________________________________________
-    cout << "Ali\tAhmed\tLina\n";
-    cout << "Fadi\tZain\tMona\n";
+     // //______________________________________________
+     // cout << "Ali\tAhmed\tLina\n";
+     // cout << "Fadi\tZain\tMona\n";
+
+    const float secondsPerDay = 24 * 60 * 60;
+    const float secondsPerHour = 60 * 60;
+    const float secondsPerMinute = 60;
+    float totalSeconds;
+    float reminder;
+    float numOfSeconds;
+    cout << "Please enter totalSeconds   : \n ";
+    cin >> totalSeconds;
+
+    float numOfDays = floor(totalSeconds / secondsPerDay);
+    reminder = fmod(totalSeconds, secondsPerDay);
+    float numOfHour = floor(reminder / secondsPerHour);
+    reminder = fmod(reminder, secondsPerHour);
+    float numOfMinutes = floor(reminder / secondsPerMinute);
+    reminder = fmod(reminder, secondsPerMinute);
+    numOfSeconds = reminder;
+    cout << numOfDays << " : " << numOfHour << ": " << numOfMinutes << ": " << numOfSeconds;
+
+
 }
