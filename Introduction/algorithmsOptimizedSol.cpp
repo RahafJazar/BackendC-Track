2d4c0346c20c9edc36f703e2af5e61913086e4ae
RahafJazar <rahafjazar73@gmail.com>
2025-10-06 14:31:18 +0300

    course 4

--- Introduction/algorithmsOptimizedSol.cpp
+++ Introduction/algorithmsOptimizedSol.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
 using namespace std;
+enum enPrinDir
+{
+    FirstNameLastName,
+    LastNameFistName
+
+};
+struct strInfo
+{
+    string FirstName;
+    string LastName;
+};
+
+strInfo readInfo()
+{
+    strInfo info;
+    cout << "Enter Your  First Name  ?" << "\n";
+    cin >> info.FirstName;
+    cout << "Enter Your Last Name " << "\n";
+    cin >> info.LastName;
+
+    return info;
+}
+
+string getFullName(strInfo info, enPrinDir printDir)
+{
+    string fullName = "";
+    if (printDir == enPrinDir::LastNameFistName)
+        fullName = info.LastName + " " + info.FirstName;
+    else
+        fullName = info.FirstName + " " + info.LastName;
+    return fullName;
+}
+
+void printFullName(string fullname)
+{
+
+    cout << "Your Full Name is: " << fullname << endl;
+}
 int main()
 {
+
+    strInfo info = readInfo();
+    string fullname = getFullName(info, enPrinDir::FirstNameLastName);
+    printFullName((fullname));
 }
