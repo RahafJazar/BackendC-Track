0b67c1eba74609e5bd268d1ac49a149687f543a7
RahafJazar <rahafjazar73@gmail.com>
2025-10-08 18:00:37 +0300

    wew

--- Introduction/algorithmsOptimizedSol.cpp
+++ Introduction/algorithmsOptimizedSol.cpp
@@ -2,35 +2,111 @@
 #include <cmath>
 using namespace std;
 
-short ReadAge()
+int ReadNumber()
 {
-    short age;
-    cout << "Enter Your Age between 18 and 45 ? " << "\n";
-    cin >> age;
-    return age;
-}
-bool validateNumberInRange(short number, short from, short to)
-{
-    return (number >= from && number <= to);
-}
 
-short ReadUntiAgeBetween(short from, short to)
+    int number;
+    cout << "enter Scope of number to print" << endl;
+    cin >> number;
+    return number;
+}
+void printReversedNumbersNto1_UsingFor(int N)
 {
-    short age = 0;
+    cout << "****************************************" << endl;
+    for (int i = N; i >= 1; i--)
+    {
+        cout << i << "\n";
+    }
+    cout << "****************************************" << endl;
+}
+void printReversedNumbersNto1_UsingWhile(int N)
+{
+    cout << "****************************************" << endl;
+    int i = N;
+    while (i >= 1)
+    {
+        cout << i << "\n";
+        i--;
+    }
+    cout << "****************************************" << endl;
+}
+void printReversedNumbersNto1_UsingDoWhile(int N)
+{
+    cout << "****************************************" << endl;
+    int i = N;
     do
     {
-        age = ReadAge();
-    } while (!validateNumberInRange(age, from, to));
+        cout << i << "\n";
+        i--;
+    } while (i >= 1);
 
-    return age;
+    cout << "****************************************" << endl;
 }
 
-void PrintResult(short age)
+enum enOddOrEven
 {
-    cout << "\n your age is : " << age << endl;
+    Odd = 1,
+    Even = 2
+};
+int ReadNumber()
+{
+
+    int number;
+    cout << "enter Scope of number to print" << endl;
+    cin >> number;
+    return number;
 }
+enOddOrEven CheckOddOrEven(int number)
+{
+    if (number % 2 == 0)
+        return enOddOrEven::Even;
+    else
+        return enOddOrEven ::Odd;
+}
+int sumOddNumbersFrom1toN_UsingFor(int N)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (CheckOddOrEven(i) == enOddOrEven::Odd)
+        {
+            sum += i;
+        }
+    }
+    return sum;
+}
+int sumOddNumbersFrom1toN_UsingWhile(int N)
+{
+    int sum = 0;
+    int i = 1;
+    while (i <= N)
+    {
+        if (CheckOddOrEven(i) == enOddOrEven::Odd)
+        {
+            sum += i;
+        }
+    }
+    return sum;
+}
+int sumOddNumbersFrom1toN_UsingDoWhile(int N)
+{
+    int sum = 0;
+    int i = 1;
+    do
+    {
+        if (CheckOddOrEven(i) == enOddOrEven::Odd)
+        {
+            sum += i;
+        }
+    } while (i <= N);
+
+    return sum;
+}
+
 int main()
 {
-
-    PrintResult(ReadUntiAgeBetween(18, 45));
+    int selectedNumber = ReadNumber();
+    int num = sumOddNumbers(selectedNumber);
+    cout << "Sum of Odd of " << selectedNumber << "Is : " << num;
 }
