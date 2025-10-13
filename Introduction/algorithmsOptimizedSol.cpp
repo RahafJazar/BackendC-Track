20664f8425dd31c808d64f73ad8ebe2e3371c94a
RahafJazar <rahafjazar73@gmail.com>
2025-10-13 11:20:39 +0300

    until 40

--- Introduction/algorithmsOptimizedSol.cpp
+++ Introduction/algorithmsOptimizedSol.cpp
@@ -2,29 +2,33 @@
 #include <cmath>
 using namespace std;
 
-enum enPrimeNotPrime
-{
-    Prime = 1,
-    NotPrime = 2
-};
-float ReadPositiveNumbers(string message)
+float ReadPositiveNumber(string message)
 {
     float num;
     do
     {
-
         cout << message << endl;
         cin >> num;
-        return num;
+
     } while (num <= 0);
+    return num;
 }
-enPrimeNotPrime checkPrime(int number)
+
+float TotalBillAfterTaxAndServiceFee(float billValue)
 {
-    
+
+    billValue += (billValue * 0.1);
+
+    billValue += (billValue * 0.16);
+
+    return billValue;
 }
 int main()
 {
+    float billValue = ReadPositiveNumber("Please enter  Bill Value :");
 
-    float result = sumNmbers();
-    cout << "Result is : " << result;
+    cout << "Total Bill before is : " << billValue << endl;
+
+    float billValueAfter = TotalBillAfterTaxAndServiceFee(billValue);
+    cout << "Total Bill After Service and tax fee , bill will be : " << billValueAfter << "\n";
 }
