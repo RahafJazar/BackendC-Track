975687be4edd0bec021dbb3afcd55b4931fd75dc
RahafJazar <rahafjazar73@gmail.com>
2025-10-13 17:55:48 +0300

    49

--- Introduction/algorithmsOptimizedSol.cpp
+++ Introduction/algorithmsOptimizedSol.cpp
@@ -1,34 +1,61 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-float ReadPositiveNumber(string message)
+enum enDaysOfWeek
 {
-    float num;
+    Sun = 1,
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat
+};
+int ReadNumberInRange(string message, int from, int to)
+{
+    int num;
     do
     {
         cout << message << endl;
         cin >> num;
 
-    } while (num <= 0);
+    } while (num < from || num > to);
     return num;
 }
-
-float TotalBillAfterTaxAndServiceFee(float billValue)
+enDaysOfWeek ReadDayOfWeek()
 {
-
-    billValue += (billValue * 0.1);
-
-    billValue += (billValue * 0.16);
-
-    return billValue;
+    return (enDaysOfWeek)ReadNumberInRange("please enter day number sun=1,mon=2,...,sat=7", 1, 7);
 }
+string GetDayOfWeek(enDaysOfWeek daysOfWeek)
+{
+    switch (daysOfWeek)
+    {
+    case enDaysOfWeek::Sun:
+        return "Sunday";
+        break;
+    case enDaysOfWeek::Mon:
+        return "Monday";
+        break;
+    case enDaysOfWeek::Tue:
+        return "Tuesday";
+        break;
+    case enDaysOfWeek::Wed:
+        return "Wednesday";
+        break;
+    case enDaysOfWeek::Thu:
+        return "Thursday";
+        break;
+    case enDaysOfWeek::Fri:
+        return "Friday";
+        break;
+    case enDaysOfWeek::Sat:
+        return "Saturday";
+        break;
+    }
+}
+
 int main()
 {
-    float billValue = ReadPositiveNumber("Please enter  Bill Value :");
-
-    cout << "Total Bill before is : " << billValue << endl;
-
-    float billValueAfter = TotalBillAfterTaxAndServiceFee(billValue);
-    cout << "Total Bill After Service and tax fee , bill will be : " << billValueAfter << "\n";
+    string day = GetDayOfWeek(ReadDayOfWeek());
+    cout << "day is  : " << day << endl;
 }
