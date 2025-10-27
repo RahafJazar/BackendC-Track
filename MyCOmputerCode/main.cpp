52e2f5d65bd33a895c7b46b3087474174654da2d
RahafJazar <rahafjazar73@gmail.com>
2025-10-27 17:56:53 +0300

    sdsd

--- MyCOmputerCode/main.cpp
+++ MyCOmputerCode/main.cpp
@@ -4,15 +4,45 @@
 
 using namespace std;
 
+enum enRandomNumberType{
+    SmallLetter=1,
+    CapitalLetter,
+    SpecialCharacter,
+    Digit
+};
 int RandomNumber(int from, int to)
 {
     // rand -> from 0 to max
     return rand() % (to - from + 1) + from;
 }
 
+char GetRandomCharacter(enRandomNumberType charType){
+    switch (charType)
+    {
+    case enRandomNumberType::SmallLetter:
+        return RandomNumber(97,122);
+        break;
+    case enRandomNumberType::CapitalLetter:
+        return RandomNumber(65,90);
+        break;
+    case enRandomNumberType::SpecialCharacter:
+        return RandomNumber(33,47);
+        break;
+    case enRandomNumberType::Digit:
+        return RandomNumber(48,57);
+        break;
+    
+    default:
+        break;
+    }
+}
+
 int main()
 {
     srand((unsigned)time(NULL));
-    cout <<RandomNumber(1,5)<<endl;
-    cout <<RandomNumber(1,5)<<endl;
+    cout << GetRandomCharacter(enRandomNumberType::SmallLetter) << endl;
+    cout << GetRandomCharacter(enRandomNumberType::CapitalLetter) << endl;
+    cout << GetRandomCharacter(enRandomNumberType::SpecialCharacter) << endl;
+    cout << GetRandomCharacter(enRandomNumberType::Digit) << endl;
+ 
 }
