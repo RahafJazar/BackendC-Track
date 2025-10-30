aae38f20b8e1be43c28104e2958c17b24d225d8b
RahafJazar <rahafjazar73@gmail.com>
2025-10-30 17:57:22 +0300

    22

--- MyCOmputerCode/main.cpp
+++ MyCOmputerCode/main.cpp
@@ -4,27 +4,56 @@
 
 using namespace std;
 
-int ReadNumber()
+int ReadNumber(string msg)
 {
-    cout << "please Enter number of generated keys";
+    int n;
+    cout << msg << endl;
+    cin >> n;
+    return n;
 }
 
-int RandomNumber(int from, int to)
+void ReadArray(int arr[100], int &lengthOfArr)
 {
-    // rand -> from 0 to max
-    return rand() % (to - from + 1) + from;
-}
 
-string generateKey()
-{
-    string s = "";
-    for (int i = 0; i < 4; i++)
+    lengthOfArr = ReadNumber("Enter Number Of elements");
+    for (int i = 0; i < lengthOfArr; i++)
     {
-        s += char(RandomNumber(33, 47));
+
+        cout << "Element [" << i + 1 << "] : ";
+        cin >> arr[i];
     }
+}
+void PrintArr(int arr[100], int &lengthOfArr)
+{
+    for (int i = 0; i < lengthOfArr; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int TimesRepeated(int number, int lenthOfArray, int arr[100])
+{
+    int count = 0;
+    for (int i = 0; i < lenthOfArray; i++)
+    {
+        if (arr[i] == number)
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 int main()
 {
-    srand((unsigned)time(NULL));
+    int arr[100], numToCheck, numOfElements;
+
+    ReadArray(arr, numOfElements);
+
+    cout << "Original Array  : ";
+    PrintArr(arr, numOfElements);
+
+    numToCheck = ReadNumber("Enter the number to check ");
+
+    cout << "Number " << numToCheck << " is repeaed " << TimesRepeated(numToCheck, numOfElements, arr) << " time(s) \n";
 }
