8b19e6ad91f3885e2074fce556bec5e8180a7932
RahafJazar <rahafjazar73@gmail.com>
2025-11-04 11:48:07 +0300

    AlgoAdvanced

--- MyCOmputerCode/main.cpp
+++ MyCOmputerCode/main.cpp
@@ -5,46 +5,21 @@
 
 using namespace std;
 
-int ReadPositiveNumber(string msg)
+float ReadPositiveNumber(string msg)
 {
-    int n;
+    float n;
     cout << msg << endl;
     cin >> n;
     return n;
 }
-
-void FillArray(int arr[100], int &arrLength)
+float MySqrt(float number)
 {
-    arrLength = ReadPositiveNumber("Enter Number of array length");
-    for (int i = 0; i < arrLength; i++)
-    {
-        arr[i] = ReadPositiveNumber("Enter a number ?");
-    }
+    return pow(number, 0.5); // ي ؤقم للقوة نص  هو  الجذر  التربيعي
 }
- 
-void PrintArray(int arr[100], int sizeOfArr)
-{
-    for (int i = 0; i < sizeOfArr; i++)
-    {
-        cout << arr[i] << " ";
-    }
-}
-
-bool IsPalindromeArray(int arr[100], int sizeOfArr){
-    
-}
-
 int main()
 {
-    srand((unsigned)time(NULL));
-    int arr1[100], arr2[100], arr1Length = 0, arr2Length = 0;
 
-    FillArray(arr1, arr1Length);
-    cout << "\nArray1 Elements : ";
-    PrintArray(arr1, arr1Length);
-
-    CopyDistinctNumberToArray(arr1, arr2, arr1Length, arr2Length);
-
-    cout << "\nArray2 Distinct Element :";
-    PrintArray(arr2, arr2Length);
+    float num = ReadPositiveNumber("Please Enter  number ? ");
+    cout << "My  sqrt  Result : " << MySqrt(num) << endl;
+    cout << "C++ Sqrt Result : " << sqrt(num) << endl;
 }
