11127da87b1eb0f5cad3ef591d21bf9c3313baff
RahafJazar <rahafjazar73@gmail.com>
2025-12-07 17:57:46 +0300

    Vector functions

--- MyCOmputerCode/c++level2.cpp
+++ MyCOmputerCode/c++level2.cpp
@@ -16,4 +16,13 @@ int main()
     vector<MyLib::stEmployee> vEmployees;
     MyLib::ReadEmployeesVector(vEmployees);
     MyLib::PrintEmployeesVector(vEmployees);
+
+    cout << setw(10) << "First element of victor : " << endl;
+    vEmployees.front();
+    cout << setw(10) << "Last element of victor : " << endl;
+    vEmployees.back();
+
+    vEmployees.pop_back();
+    cout << setw(40) << "VEmplyees after pop_pack() function is applied " << endl;
+    MyLib::PrintEmployeesVector(vEmployees);
 }
