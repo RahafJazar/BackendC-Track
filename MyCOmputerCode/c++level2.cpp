f8b7444b28e61a608675f265ba5dca81cf9d7d66
RahafJazar <rahafjazar73@gmail.com>
2026-01-21 17:59:56 +0300

    c++

--- MyCOmputerCode/c++level2.cpp
+++ MyCOmputerCode/c++level2.cpp
@@ -9,20 +9,118 @@
 #include "HeaderFiles/StudentLib.h"
 #include "HeaderFiles/MathLib.h"
 
-using namespace MyInputLib;
+// time management
+#include <chrono>
 
+struct stStudent
+{
+    string Name;
+    int Age;
+    string Major;
+};
+using namespace MyInputLib;
+void SaveAddressinPointerStack(int number);
+void SaveAddressinPointerHeap(int number);
+void PointerWithStructStack(stStudent st);
+void PointerWithArray(int arr[], int size);
+void UpdateByReference(int &num);
+void UpdateLocal(int num);
+void DateAndTime();
 int main()
 {
-    vector<MyLib::stEmployee> vEmployees;
-    MyLib::ReadEmployeesVector(vEmployees);
-    MyLib::PrintEmployeesVector(vEmployees);
+    // vector<MyLib::stEmployee> vEmployees;
+    // MyLib::ReadEmployeesVector(vEmployees);
+    // MyLib::PrintEmployeesVector(vEmployees);
 
-    cout << setw(10) << "First element of victor : " << endl;
-    vEmployees.front();
-    cout << setw(10) << "Last element of victor : " << endl;
-    vEmployees.back();
+    // cout << setw(10) << "First element of victor : " << endl;
+    // vEmployees.front();
+    // cout << setw(10) << "Last element of victor : " << endl;
+    // vEmployees.back();
 
-    vEmployees.pop_back();
-    cout << setw(40) << "VEmplyees after pop_pack() function is applied " << endl;
-    MyLib::PrintEmployeesVector(vEmployees);
+    // vEmployees.pop_back();
+    // cout << setw(40) << "VEmplyees after pop_pack() function is applied " << endl;
+    // MyLib::PrintEmployeesVector(vEmployees);
+    auto start = std::chrono::high_resolution_clock::now();
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+    cout << sum << endl;
+    SaveAddressinPointerStack(10);
+    SaveAddressinPointerHeap(10);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    cout << "Execution time is : " << duration.count() << " seconds " << endl;
+
+    stStudent student1;
+    student1.Age = 34;
+    student1.Major = "Cs";
+    student1.Name = "Ahmad";
+    PointerWithStructStack(student1);
+
+    int arr[4] = {45, 345, 4, 2};
+    PointerWithArray(arr, 4);
+
+    int x1 = 5, y1 = 5;
+    UpdateByReference(x1);
+    UpdateLocal(y1);
+    cout << "x1 after updating by reference  : " << x1 << endl;
+    cout << "y1 after updating by local : " << y1 << endl;
+
+    DateAndTime();
+}
+void SaveAddressinPointerStack(int number) // pointer and value in the stack
+{
+    int *p = &number;
+    cout << "address of " << number << "=" << p << endl;
+    cout << "value of p :  " << "=" << *p << endl;
+    *p = 30; // dereferencing
+    cout << "address  in p : " << p << endl;
+    cout << "value of p: " << *p << endl;
+    cout << "value of number is : " << number << endl;
+}
+void SaveAddressinPointerHeap(int number) // pointer and value in the stack
+{
+    int *p = new int(6); // new => heap , you must to clean the memory after usage to prevent memory leak happen
+    cout << "address of p " << p << endl;
+    cout << "value of p :  " << *p << endl;
+    *p = 30; // dereferencing
+    cout << "address  in p : " << p << endl;
+    cout << "value of p: " << *p << endl;
+    delete p;    // free heap memory
+    p = nullptr; // safety
+}
+
+void PointerWithStructStack(stStudent s)
+{
+    stStudent *ps = &s;
+    cout << "Age: " << ps->Age << endl;
+    cout << "Major: " << ps->Major << endl;
+    cout << "Name: " << ps->Name << endl;
+    cout << "Address in p is : " << ps << endl;
+}
+
+void PointerWithArray(int arr2[], int size)
+{
+    int *p = arr2;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "p[" << i << "] = " << *(p + i) << endl;
+    }
+}
+
+void UpdateByReference(int &num)
+{
+    num = 61;
+}
+void UpdateLocal(int num)
+{
+    num = 61;
+}
+
+void DateAndTime()
+{
+    time_t now = time(0); // num of seconds since 1970
+    cout << ctime(&now);
+    tm *t = localtime(&now);
+    cout << t->
 }
