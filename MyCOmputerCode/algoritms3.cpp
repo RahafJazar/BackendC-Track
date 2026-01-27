3b11a89e4213c97df833c5118d732cfc3bfae6b2
RahafJazar <rahafjazar73@gmail.com>
2026-01-27 17:50:14 +0300

    leetcode

--- MyCOmputerCode/algoritms3.cpp
+++ MyCOmputerCode/algoritms3.cpp
@@ -89,30 +89,60 @@ void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     int i = m - 1;
     int j = n - 1;
     // if n>0 && m>0
-    
-        while (j >= 0)
+
+    while (j >= 0)
+    {
+
+        if (i >= 0 && nums1[i] > nums2[j])
         {
-
-            if (i >= 0 && nums1[i] > nums2[j])
-            {
-                nums1[k] = nums1[i];
-                i--;
-            }
-            else
-            {
-                nums1[k] = nums2[j];
-                j--;
-            }
-
-            k--;
+            nums1[k] = nums1[i];
+            i--;
+        }
+        else
+        {
+            nums1[k] = nums2[j];
+            j--;
         }
 
-   
+        k--;
+    }
+}
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    // his syntax : val(x), left(nullptr) ? That’s called an "initializer list".
+    TreeNode() : val(0), left(nullptr), right(nullptr) {};      // it creates an empty node with val 0 and  no children;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}; // it creates an node with val x and no children;
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {};
+};
+TreeNode *sortedArrayToBSTRecur(vector<int> &nums, int start, int end)
+{
+    if (start > end)
+    {
+        return nullptr;
+    };
+    int mid = (start + end) / 2;
+    TreeNode *root = new TreeNode(nums[mid]);
+    root->left = sortedArrayToBSTRecur(nums, start, mid - 1);
+    root->right = sortedArrayToBSTRecur(nums, mid + 1, end);
+    return root;
+}
+TreeNode *sortedArrayToBST(vector<int> &nums)
+{
+    int start = 0;
+    int end = nums.size() - 1;
+    return sortedArrayToBSTRecur(nums, start, end);
+}
+void PrintLevelOrder(TreeNode *root){
+    
 }
 int main()
 {
 
-    vector<int> nums1 = {0}; // Input array
-    vector<int> nums2 = {1};    // Input array
-    merge(nums1,0, nums2, 1);
+    vector<int> nums1 = {10, -3, 0, 5, 9}; // Input array
+    TreeNode *res=new TreeNode();
+    res= sortedArrayToBST(nums1);
 }
