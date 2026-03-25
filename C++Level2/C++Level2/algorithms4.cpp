02592aad64c1c7ae0e1d397b8a7396c7167c8b1f
RahafJazar <rahafjazar73@gmail.com>
2026-03-25 14:24:42 +0300

    algo4

--- C++Level2/C++Level2/algorithms4.cpp
+++ C++Level2/C++Level2/algorithms4.cpp
@@ -3,32 +3,37 @@
 
 using namespace std;
 
-int ReadNumber()
+long ReadNumber()
 {
-    int n;
-    cout << "Enter a  number ?";
+    long n;
+    cout << "\nEnter a  number ?";
     cin >> n;
     cin.ignore();
     return n;
 }
-string ConvertNumberToText(int number)
+string ConvertNumberToText(long n)
 {
     if (n == 0)
     {
         return "";
     }
+    if (n >= 1000000000)
+    {
+        /* code */
+        return ConvertNumberToText(n / 1000000000) + " " + "Billion" + " " + ConvertNumberToText(n % 1000000000);
+    }
     if (n >= 1000000)
     {
         /* code */
-        return ConvertNumberToText(n / 1000000) + " Millons " + ConvertNumberToText(n % 1000000);
+        return ConvertNumberToText(n / 1000000) + " " + "Million" + " " + ConvertNumberToText(n % 1000000);
     }
     if (n >= 1000)
     {
-        return ConvertNumberToText(n / 1000) + " "+"Thousands" +" "+ ConvertNumberToText(n % 1000);
+        return ConvertNumberToText(n / 1000) + " " + "Thousand" + " " + ConvertNumberToText(n % 1000);
     }
     if (n >= 100)
     {
-        return ConvertNumberToText(n / 100) + " "+ "Hundreds" +" "+ ConvertNumberToText(n % 100);
+        return ConvertNumberToText(n / 100) + " " + "Hundred" + " " + ConvertNumberToText(n % 100);
     }
     if (n >= 20 && n <= 99)
     {
@@ -38,19 +43,52 @@ string ConvertNumberToText(int number)
     if (n >= 1 && n <= 19)
     {
         string arr[] = {
-                           "", "One", "Two", "Three", "Four", "Five",
-                           "Six", "Seven", "Eight", "Nine", "Ten",
-                           "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
-                           "Sixteen", "Seventeen", "Eighteen", "Nineteen"} return arr[n] +
-                       " ";
+            "", "One", "Two", "Three", "Four", "Five",
+            "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+            "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+
+        return arr[n] + " ";
     }
+    return "";
+}
+short ReadYear()
+{
+    shory year;
+    cout << "\nEnter a  Year to check  ?";
+    cin >> year;
+    cin.ignore();
+    return year;
+}
+bool IsLeapYear(short year)
+{
+    return ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0));
 }
 
+short NumberOFDaysInYear(short year)
+{
+    return IsLeapYear(year) ? 366 : 365;
+}
+
+short NumberOfHoursInYear(short Year)
+{
+}
 int main()
 {
-    int number = ReadNumber();
+    // 1-convert number to text
+    long number = ReadNumber();
     cout << ConvertNumberToText(number);
 
+    // 2-leap year
+    short year = ReadYear();
+    if (IsLeapYear(year))
+    {
+        cout << "\nYes , Year [" << year << "] is a leap year.\n";
+    }
+    else
+    {
+        cout << "\nNo , Year [" << year << "] isn't  a leap year.\n";
+    }
     system("pause>0");
     return 0;
 }
